edc23938cac06870ef67970df3bd1249b662d884
yanai <yanai@tikalk.com>
2018-02-22 09:35:12 +0200

    kinesis

--- handler.js
+++ handler.js
@@ -25,20 +25,12 @@ exports.publishPlayerScoreEventsHandler = function (event, context, callback) {
     const eventStr = JSON.stringify(event);
     console.log("Got Players Table Event from stream : "+eventStr);
     const newImage = event.Records.map(record => record.dynamodb.NewImage);
-    faas_grip.publish('mychannel', new grip.HttpStreamFormat(
+    faas_grip.publish('test', new grip.HttpStreamFormat(
         'event: message\ndata: '+JSON.stringify(newImage)+'\n\n'));
     callback(null, "Successfully processed "+eventStr);
 }
 
 
 
-exports.publishTeamScoreEventsHandler = function (event, context, callback) {
-    const eventStr = JSON.stringify(event);
-    console.log("Got Teams Table Event from stream : "+eventStr);
-    const newImage = event.Records.map(record => record.dynamodb.NewImage);
-    faas_grip.publish('mychannel', new grip.HttpStreamFormat(
-        'event: message\ndata: '+JSON.stringify(newImage)+'\n\n'));
-    callback(null, "Successfully processed "+eventStr);
-}
 
 
